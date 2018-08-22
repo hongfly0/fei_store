9f8e9a2101c8315f5482589bc66dae0ad28b2c0c
Fable <278380854@qq.com>
2018-08-22 22:26:21 +0800

    主页创建

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,120 +1,201 @@
-//index.js
 const app = getApp()
 
 Page({
+
   data: {
-    avatarUrl: './user-unlogin.png',
-    userInfo: {},
-    logged: false,
-    takeSession: false,
-    requestResult: ''
+    swiperCurrent: 0,
+
+    indicatorDots: true,
+
+    autoplay: true,
+
+    interval: 3000,
+
+    duration: 800,
+
+    circular: true,
+
+    imgUrls: [
+      'https://p3.pstatp.com/large/43700001e49d85d3ab52',
+      'https://p3.pstatp.com/large/39f600038907bf3b9c96',
+      'https://p3.pstatp.com/large/31fa0003ed7228adf421'
+    ],
+    links: [
+      '../menu/menu',
+      '../menu/menu',
+      '../menu/menu'
+    ],
+    scrollH: 0,
+    imgWidth: 0,
+    loadingCount: 0,
+    images: [],
+    col1: [],
+    col2: []
   },
 
-  onLoad: function() {
-    if (!wx.cloud) {
-      wx.redirectTo({
-        url: '../chooseLib/chooseLib',
-      })
-      return
+  onLoad: function () {
+    wx.getSystemInfo({
+      success: (res) => {
+        let ww = res.windowWidth;
+        let wh = res.windowHeight;
+        let imgWidth = ww * 0.48;
+        let scrollH = wh;
+
+        this.setData({
+          scrollH: scrollH,
+          imgWidth: imgWidth
+        });
+
+        //加载首组图片
+        this.loadImages();
+      }
+    })
+  },
+
+  onImageLoad: function (e) {
+    let imageId = e.currentTarget.id;
+    let oImgW = e.detail.width;         //图片原始宽度
+    let oImgH = e.detail.height;        //图片原始高度
+    let imgWidth = this.data.imgWidth;  //图片设置的宽度
+    let scale = imgWidth / oImgW;        //比例计算
+    let imgHeight = oImgH * scale;      //自适应高度
+
+    let images = this.data.images;
+    let imageObj = null;
+
+    for (let i = 0; i < images.length; i++) {
+      let img = images[i];
+      if (img.id === imageId) {
+        imageObj = img;
+        break;
+      }
     }
 
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              this.setData({
-                avatarUrl: res.userInfo.avatarUrl,
-                userInfo: res.userInfo
-              })
-            }
-          })
-        }
-      }
-    })
-  },
+    imageObj.height = imgHeight;
 
-  onGetUserInfo: function(e) {
-    if (!this.logged && e.detail.userInfo) {
-      this.setData({
-        logged: true,
-        avatarUrl: e.detail.userInfo.avatarUrl,
-        userInfo: e.detail.userInfo
-      })
+    let loadingCount = this.data.loadingCount - 1;
+    let col1 = this.data.col1;
+    let col2 = this.data.col2;
+
+    //判断当前图片添加到左列还是右列
+    if (col1.length <= col2.length) {
+      col1.push(imageObj);
+    } else {
+      col2.push(imageObj);
     }
+
+    let data = {
+      loadingCount: loadingCount,
+      col1: col1,
+      col2: col2
+    };
+
+    //当前这组图片已加载完毕，则清空图片临时加载区域的内容
+    if (!loadingCount) {
+      data.images = [];
+    }
+
+    this.setData(data);
   },
 
-  onGetOpenid: function() {
-    // 调用云函数
-    wx.cloud.callFunction({
-      name: 'login',
-      data: {},
-      success: res => {
-        console.log('[云函数] [login] user openid: ', res.result.openid)
-        app.globalData.openid = res.result.openid
-        wx.navigateTo({
-          url: '../userConsole/userConsole',
-        })
+  loadImages: function () {
+    let images = [
+      {
+        goodId: 0,
+        name: '泊尔崖蜜蜜光面膜（5片盒装）',
+        url: 'bill',
+        imageurl: 'https://a3.vimage1.com/upload/merchandise/pdcvis/2017/08/21/142/fb2960bf8e074d029c24315279289c19-5_218x274_70.jpg',
+        newprice: "86",
+        oldprice: "88",
+        discount: "8.8",
+        height: 0,
       },
-      fail: err => {
-        console.error('[云函数] [login] 调用失败', err)
-        wx.navigateTo({
-          url: '../deployFunctions/deployFunctions',
-        })
-      }
-    })
-  },
-
-  // 上传图片
-  doUpload: function () {
-    // 选择图片
-    wx.chooseImage({
-      count: 1,
-      sizeType: ['compressed'],
-      sourceType: ['album', 'camera'],
-      success: function (res) {
-
-        wx.showLoading({
-          title: '上传中',
-        })
-
-        const filePath = res.tempFilePaths[0]
-        
-        // 上传图片
-        const cloudPath = 'my-image' + filePath.match(/\.[^.]+?$/)[0]
-        wx.cloud.uploadFile({
-          cloudPath,
-          filePath,
-          success: res => {
-            console.log('[上传文件] 成功：', res)
-
-            app.globalData.fileID = res.fileID
-            app.globalData.cloudPath = cloudPath
-            app.globalData.imagePath = filePath
-            
-            wx.navigateTo({
-              url: '../storageConsole/storageConsole'
-            })
-          },
-          fail: e => {
-            console.error('[上传文件] 失败：', e)
-            wx.showToast({
-              icon: 'none',
-              title: '上传失败',
-            })
-          },
-          complete: () => {
-            wx.hideLoading()
-          }
-        })
-
+      {
+        goodId: 1,
+        name: '透无瑕矿物养护两用粉饼#03',
+        url: 'bill',
+        imageurl: 'https://a.appsimg.com/upload/brand/upcb/2017/10/26/77/ias_150899004322921_604x290_80.jpg!75.webp',
+        newprice: "147.00",
+        oldprice: "150.00",
+        discount: "8.8",
+        height: 0,
       },
-      fail: e => {
-        console.error(e)
+      {
+        goodId: 2,
+        name: '川水水光面膜（5片盒装）',
+        url: 'bill',
+        imageurl: 'https://a2.vimage1.com/upload/merchandise/pdcvis/2017/08/21/86/7891361fdab348a1bc91aeca31fc77b1-5_218x274_70.jpg',
+        newprice: "86.00",
+        oldprice: "88.00",
+        discount: "8.8",
+        height: 0,
+      },
+      {
+        goodId: 3,
+        name: '蜜三色渐变咬唇膏3.2g 03蜜橙动心恋',
+        url: 'bill',
+        imageurl: 'http://a3.vimage1.com/upload/merchandise/pdcvis/2017/08/21/176/c3b9453a4d7f46c6a8fe78705f77352b-5_218x274_70.jpg',
+        newprice: "97.00",
+        oldprice: "99.00",
+        discount: "8.8",
+        height: 0,
+      },
+      {
+        goodId: 4,
+        name: '时焕颜亮采套装',
+        url: 'bill',
+        imageurl: 'https://a2.vimage1.com/upload/merchandise/pdcvis/2017/08/21/93/69a6bc1c11eb4be184b7dffb43b8565b-5_218x274_70.jpg',
+        newprice: "398.00",
+        oldprice: "459.00",
+        discount: "8.8",
+        height: 0,
       }
-    })
+    ];
+
+    let baseId = "img-" + (+new Date());
+
+    for (let i = 0; i < images.length; i++) {
+      images[i].id = baseId + "-" + i;
+    }
+
+    this.setData({
+      loadingCount: images.length,
+      images: images
+    });
   },
 
+swiperChange: function (e) {
+
+    this.setData({
+
+      swiperCurrent: e.detail.current
+
+    })
+
+  },
+
+  //点击指示点切换
+
+  chuangEvent: function (e) {
+
+    this.setData({
+
+      swiperCurrent: e.currentTarget.id
+
+    })
+
+  },
+
+  //点击图片触发事件
+
+  swipclick: function (e) {
+
+    console.log(this.data.swiperCurrent);
+
+    wx.switchTab({
+
+      url: this.data.links[this.data.swiperCurrent]
+
+    })
+  }
 })
