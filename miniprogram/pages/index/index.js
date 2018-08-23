a28bb02ff56166899734289e90f9e453a2d81c6f
Fable <278380854@qq.com>
2018-08-23 23:06:59 +0800

    主页修改

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -39,11 +39,13 @@ Page({
         let ww = res.windowWidth;
         let wh = res.windowHeight;
         let imgWidth = ww * 0.48;
+        let imgHeight  =  wh * 0.20;
         let scrollH = wh;
 
         this.setData({
           scrollH: scrollH,
-          imgWidth: imgWidth
+          imgWidth: imgWidth,
+          imgHeight: imgHeight
         });
 
         //加载首组图片
@@ -57,8 +59,10 @@ Page({
     let oImgW = e.detail.width;         //图片原始宽度
     let oImgH = e.detail.height;        //图片原始高度
     let imgWidth = this.data.imgWidth;  //图片设置的宽度
-    let scale = imgWidth / oImgW;        //比例计算
+    let dataheight = this.data.imgHeight ;
+    let scale = dataheight / oImgH;        //比例计算
     let imgHeight = oImgH * scale;      //自适应高度
+    let setwidth = oImgW * scale;
 
     let images = this.data.images;
     let imageObj = null;
@@ -71,23 +75,17 @@ Page({
       }
     }
 
-    imageObj.height = imgHeight;
+    imageObj.height = dataheight;
+    imageObj.width = setwidth;
 
     let loadingCount = this.data.loadingCount - 1;
     let col1 = this.data.col1;
-    let col2 = this.data.col2;
 
-    //判断当前图片添加到左列还是右列
-    if (col1.length <= col2.length) {
-      col1.push(imageObj);
-    } else {
-      col2.push(imageObj);
-    }
+    col1.push(imageObj);
 
     let data = {
       loadingCount: loadingCount,
-      col1: col1,
-      col2: col2
+      col1: col1
     };
 
     //当前这组图片已加载完毕，则清空图片临时加载区域的内容
@@ -109,7 +107,49 @@ Page({
         oldprice: "88",
         discount: "8.8",
         height: 0,
+      }
+      ,
+      {
+        goodId: 1,
+        name: '透无瑕矿物养护两用粉饼#03',
+        url: 'bill',
+        imageurl: 'https://a.appsimg.com/upload/brand/upcb/2017/10/26/77/ias_150899004322921_604x290_80.jpg!75.webp',
+        newprice: "147.00",
+        oldprice: "150.00",
+        discount: "8.8",
+        height: 0,
       },
+      {
+        goodId: 2,
+        name: '川水水光面膜（5片盒装）',
+        url: 'bill',
+        imageurl: 'https://a2.vimage1.com/upload/merchandise/pdcvis/2017/08/21/86/7891361fdab348a1bc91aeca31fc77b1-5_218x274_70.jpg',
+        newprice: "86.00",
+        oldprice: "88.00",
+        discount: "8.8",
+        height: 0,
+      },
+      {
+        goodId: 3,
+        name: '蜜三色渐变咬唇膏3.2g 03蜜橙动心恋',
+        url: 'bill',
+        imageurl: 'http://a3.vimage1.com/upload/merchandise/pdcvis/2017/08/21/176/c3b9453a4d7f46c6a8fe78705f77352b-5_218x274_70.jpg',
+        newprice: "97.00",
+        oldprice: "99.00",
+        discount: "8.8",
+        height: 0,
+      },
+      {
+        goodId: 4,
+        name: '时焕颜亮采套装',
+        url: 'bill',
+        imageurl: 'https://a2.vimage1.com/upload/merchandise/pdcvis/2017/08/21/93/69a6bc1c11eb4be184b7dffb43b8565b-5_218x274_70.jpg',
+        newprice: "398.00",
+        oldprice: "459.00",
+        discount: "8.8",
+        height: 0,
+      }
+      ,
       {
         goodId: 1,
         name: '透无瑕矿物养护两用粉饼#03',
@@ -162,40 +202,5 @@ Page({
       loadingCount: images.length,
       images: images
     });
-  },
-
-swiperChange: function (e) {
-
-    this.setData({
-
-      swiperCurrent: e.detail.current
-
-    })
-
-  },
-
-  //点击指示点切换
-
-  chuangEvent: function (e) {
-
-    this.setData({
-
-      swiperCurrent: e.currentTarget.id
-
-    })
-
-  },
-
-  //点击图片触发事件
-
-  swipclick: function (e) {
-
-    console.log(this.data.swiperCurrent);
-
-    wx.switchTab({
-
-      url: this.data.links[this.data.swiperCurrent]
-
-    })
-  }
+  }  
 })
